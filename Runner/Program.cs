ae62ee9ed4e1aef40851cceb467d4d35201aa4a8
marko.silla@ericsson.com <marko.silla@ericsson.com>
2017-12-10 15:20:23 +0200

    First skeleton for Runner

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -8,8 +8,24 @@ namespace Runner
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            Console.WriteLine("Please choose below:");
+            Console.WriteLine("[1] SumOfMultiple");
+            Console.WriteLine("[2] SequenceAnalysis");
+            Console.WriteLine("Preas any other key for exit.");
+            while (true)
+            {
+                switch (Console.ReadKey().KeyChar)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    default:
+                        return 0;
+                }
+            }
         }
     }
 }
